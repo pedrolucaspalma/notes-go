d2f1b09bdf3b7e943504e3a0b31d82356aa31895
Pedro Palma <pedrolucaspalma@gmail.com>
2026-05-20 17:46:29 -0300

    Make logging more readable with JSON formatting

--- logging/logging.go
+++ logging/logging.go
@@ -2,7 +2,9 @@ package logging
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
 )
@@ -12,11 +14,14 @@ func NewLogger() (*AppLogger, error) {
 	if err != nil {
 		return nil, fmt.Errorf("opening debug file: %w", err)
 	}
+	writter := formattedJSONFileWritter{
+		destination: debugFile,
+	}
 
 	opts := &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}
-	logHandler := slog.NewJSONHandler(debugFile, opts)
+	logHandler := slog.NewJSONHandler(&writter, opts)
 
 	logger := slog.New(logHandler)
 
@@ -44,4 +49,24 @@ func (l *AppLogger) Warn(ctx context.Context, msg string, args ...any) {
 
 func (l *AppLogger) Error(ctx context.Context, msg string, args ...any) {
 	l.slogLogger.ErrorContext(ctx, msg, args...)
+}
+
+type formattedJSONFileWritter struct {
+	destination io.Writer
+}
+
+func (t *formattedJSONFileWritter) Write(p []byte) (int, error) {
+	var obj any
+	err := json.Unmarshal(p, &obj)
+	if err != nil {
+		return 0, err
+	}
+
+	identedJson, err := json.MarshalIndent(obj, "", "  ")
+	if err != nil {
+		return 0, err
+	}
+
+	identedJson = append(identedJson, '\n')
+	return t.destination.Write(identedJson)
 }
